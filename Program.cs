13aa4fbf5877f0e46c49f5f1c7fa5a17a0b286ea
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 11:46:50 +0530

    Added: Create Simple Linked List

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List");
+
+            LinkedList linked = new LinkedList();
+
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+            linked.Display();
         }
     }
 }
