92efd753ff258893b2b451cffd6ccdda8739d3fe
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 14:11:31 +0530

    Added: Search And Delete The Element and show Size

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LinkedList
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List");
+
+            LinkedList linked = new LinkedList();
+
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(40);
+            linked.Add(70);
+            linked.Display();
+            linked.Delete(40);
+            Console.Write("Final sequence\n");
+            linked.Display();
+            linked.Size();
         }
     }
 }
