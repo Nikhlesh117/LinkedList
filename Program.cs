f072b008964f2f1bdcf19cb8a2566dbdc3fd9f92
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 13:32:59 +0530

    Added: Search Element in Linked List

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LinkedList
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List");
+
+            LinkedList linked = new LinkedList();
+
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+            linked.Display();
+            linked.Search(30);
         }
     }
 }
