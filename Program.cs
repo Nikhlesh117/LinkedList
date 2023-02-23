084438689f38c2995e13250ac166ed9cf6ece866
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 12:18:00 +0530

    Added: Appended Elements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LinkedList
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List");
+
+            LinkedList linked = new LinkedList();
+
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+            linked.Display();
         }
     }
 }
