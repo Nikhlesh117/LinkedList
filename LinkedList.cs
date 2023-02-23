3294e263dbbcc316b77c0a2b69d60be12bd45d1f
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 12:03:47 +0530

    Added: Insert Elements At First

---
+++ LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = head;
+                head = temp;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("ListedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
